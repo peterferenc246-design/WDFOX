b5d65f66f7cc9c08477e89c8ddb4b07c1182e247
peterferenc246-design <peter.ferenc246@gmail.com>
2026-09-06 13:43:24 +0200

    Bust map runtime cache after zoom adjustment

--- public/cunderlik-gallery-runtime.js
+++ public/cunderlik-gallery-runtime.js
@@ -57,5 +57,5 @@
   if(!init()){var tries=0;var wait=setInterval(function(){tries++;if(init()||tries>20)clearInterval(wait);},250);}
 })();
 
-(function(){var s=document.createElement('script');s.src='/contact-map-runtime.js?v=5';s.defer=true;document.head.appendChild(s);})();
+(function(){var s=document.createElement('script');s.src='/contact-map-runtime.js?v=6';s.defer=true;document.head.appendChild(s);})();
 (function(){var s=document.createElement('script');s.src='/hero-marketing-runtime.js?v=1';s.defer=true;document.head.appendChild(s);})();
