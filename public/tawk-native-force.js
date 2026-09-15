f3ac44bd13dea6193804f3e27361b6ea483cbc06
peterferenc246-design <peter.ferenc246@gmail.com>
2026-09-15 11:08:33 +0200

    Restore native Tawk embed when widget is absent

--- public/tawk-native-force.js
+++ public/tawk-native-force.js
@@ -1,8 +1,28 @@
 (function () {
   'use strict';
 
+  var PROPERTY_ID = '6a951d52c3c46c344587662a';
+  var WIDGETS = {
+    sk: '1k1b9121q', de: '1k1bb2aln', en: '1k1bb9ast', hr: '1k1bjvbjq',
+    fr: '1k1blk6o4', it: '1k1bovo5t', pl: '1k1bp5qda', es: '1k1bp6lk5', sv: '1k1bpdngj'
+  };
+
+  function language() {
+    var lang = (document.documentElement.lang || 'sk').toLowerCase().split(/[-_]/)[0];
+    return WIDGETS[lang] ? lang : 'sk';
+  }
+
   function hasWidget() {
     return !!document.querySelector('#tawkchat-container, iframe[src*="tawk.to"], iframe[title*="chat" i]');
+  }
+
+  function showWidget() {
+    var api = window.Tawk_API;
+    if (!api) return;
+    try {
+      if (typeof api.showWidget === 'function') api.showWidget();
+      else if (typeof api.start === 'function') api.start({ showWidget: true });
+    } catch (_) {}
   }
 
   function positionWidget() {
@@ -15,16 +35,7 @@
     container.style.setProperty('z-index', '2147483647', 'important');
   }
 
-  function showWidget() {
-    var api = window.Tawk_API;
-    if (!api) return;
-    try {
-      if (typeof api.showWidget === 'function') api.showWidget();
-      else if (typeof api.start === 'function') api.start({ showWidget: true });
-    } catch (_) {}
-  }
-
-  function attach() {
+  function loadNative() {
     if (hasWidget()) {
       showWidget();
       positionWidget();
@@ -32,25 +43,47 @@
     }
 
     var api = window.Tawk_API;
-    if (api) {
+    if (!api) {
+      window.Tawk_API = {};
+      api = window.Tawk_API;
+    }
+    window.Tawk_LoadStart = window.Tawk_LoadStart || new Date();
+
+    if (!api.__WDFOX_NATIVE_ONLOAD) {
       var previousOnLoad = api.onLoad;
-      if (!api.__WDFOX_NATIVE_ATTACHED) {
-        api.__WDFOX_NATIVE_ATTACHED = true;
-        api.onLoad = function () {
-          try { if (typeof previousOnLoad === 'function') previousOnLoad(); } catch (_) {}
-          showWidget();
-          positionWidget();
-        };
-      }
-      showWidget();
+      api.__WDFOX_NATIVE_ONLOAD = true;
+      api.onLoad = function () {
+        try { if (typeof previousOnLoad === 'function') previousOnLoad(); } catch (_) {}
+        showWidget();
+        positionWidget();
+      };
+    }
+
+    var existing = document.querySelector('script[data-wdfox-tawk-native="1"]');
+    if (!existing) {
+      existing = document.createElement('script');
+      existing.async = true;
+      existing.setAttribute('data-wdfox-tawk-native', '1');
+      existing.src = 'https://embed.tawk.to/' + PROPERTY_ID + '/' + WIDGETS[language()];
+      existing.charset = 'UTF-8';
+      existing.setAttribute('crossorigin', '*');
+      (document.head || document.body || document.documentElement).appendChild(existing);
     }
   }
 
-  attach();
-  window.setTimeout(attach, 1000);
-  window.setTimeout(attach, 2500);
-  window.setTimeout(attach, 5000);
-  window.setTimeout(attach, 8000);
+  function ensure() {
+    if (!hasWidget()) loadNative();
+    else {
+      showWidget();
+      positionWidget();
+    }
+  }
+
+  ensure();
+  window.setTimeout(ensure, 1000);
+  window.setTimeout(ensure, 3000);
+  window.setTimeout(ensure, 6000);
+  window.setTimeout(ensure, 10000);
   window.setInterval(function () {
     if (hasWidget()) {
       showWidget();
